9bcb092eaae61b7c58ab6dc1714082a5f9dad0d0
heidezomp <heidezomp@protonmail.com>
2020-12-01 16:08:19 +0100

    Refactor into findAnswer1

--- 2020/day01/puzzle.zig
+++ 2020/day01/puzzle.zig
@@ -3,18 +3,22 @@ const std = @import("std");
 pub fn main() !void {
     const input = @embedFile("input.txt");
 
+    const answer1 = try findAnswer1(input, 2020);
+    try std.io.getStdOut().writer().print("Answer 1: {}\n", .{answer1});
+}
+
+fn findAnswer1(input: []const u8, sum: u32) !u32 {
     var lines = std.mem.tokenize(input, "\n");
-    const answer = outer: while (lines.next()) |line1| {
+    while (lines.next()) |line1| {
         const number1 = try std.fmt.parseUnsigned(u32, line1, 10);
 
         var remaining_lines = std.mem.tokenize(lines.rest(), "\n");
         while (remaining_lines.next()) |line2| {
             const number2 = try std.fmt.parseUnsigned(u32, line2, 10);
-            if (number1 + number2 == 2020) {
-                break :outer number1 * number2;
+            if (number1 + number2 == sum) {
+                return number1 * number2;
             }
         }
-    } else return error.AnswerNotFound;
-
-    try std.io.getStdOut().writer().print("Answer: {}\n", .{answer});
+    }
+    return error.AnswerNotFound;
 }
