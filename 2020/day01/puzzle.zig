fc6de99788b6815c9fe9379854906e6e07e99347
heidezomp <heidezomp@protonmail.com>
2020-12-01 17:56:27 +0100

    Load input file at runtime

--- 2020/day01/puzzle.zig
+++ 2020/day01/puzzle.zig
@@ -1,15 +1,37 @@
 const std = @import("std");
+const Allocator = std.mem.Allocator;
 
-pub fn main() !void {
-    const input = @embedFile("input.txt");
-    const sum = 2020;
+var general_purpose_allocator = std.heap.GeneralPurposeAllocator(.{}){};
 
+pub fn main() anyerror!void {
+    const gpa = if (std.builtin.link_libc) std.heap.raw_c_allocator else &general_purpose_allocator.allocator;
+    defer if (!std.builtin.link_libc) {
+        _ = general_purpose_allocator.deinit();
+    };
+    var arena_instance = std.heap.ArenaAllocator.init(gpa);
+    defer arena_instance.deinit();
+    const arena = &arena_instance.allocator;
+
+    const args = try std.process.argsAlloc(arena);
+    return mainArgs(gpa, arena, args);
+}
+
+fn mainArgs(gpa: *Allocator, arena: *Allocator, args: []const []const u8) !void {
+    if (args.len != 2) {
+        std.log.err("Incorrect number of arguments", .{});
+        std.log.info("Usage: {} <input_file>", .{args[0]});
+        std.process.exit(1);
+    }
+
+    const input = try std.fs.cwd().readFileAlloc(arena, args[1], 1024 * 1024);
     const stdout = std.io.getStdOut().writer();
 
-    const answer1 = try findAnswer1(input, sum);
+    const sum_to_find = 2020;
+
+    const answer1 = try findAnswer1(input, sum_to_find);
     try stdout.print("Answer 1: {}\n", .{answer1});
 
-    const answer2 = try findAnswer2(input, sum);
+    const answer2 = try findAnswer2(input, sum_to_find);
     try stdout.print("Answer 2: {}\n", .{answer2});
 }
 
