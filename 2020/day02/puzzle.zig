a261d90cca5b9c6902c16717b746143c928bdaa5
heidezomp <heidezomp@protonmail.com>
2020-12-02 14:29:01 +0100

    Day 2, part 2 complete

--- 2020/day02/puzzle.zig
+++ 2020/day02/puzzle.zig
@@ -29,8 +29,8 @@ fn mainArgs(gpa: *Allocator, arena: *Allocator, args: []const []const u8) !void 
     const answer1 = try findAnswer1(input);
     try stdout.print("Answer 1: {}\n", .{answer1});
 
-    //const answer2 = try findAnswer2(input);
-    //try stdout.print("Answer 2: {}\n", .{answer2});
+    const answer2 = try findAnswer2(input);
+    try stdout.print("Answer 2: {}\n", .{answer2});
 }
 
 fn findAnswer1(input: []const u8) !u32 {
@@ -63,6 +63,32 @@ fn findAnswer1(input: []const u8) !u32 {
     return valid_passwords;
 }
 
+fn findAnswer2(input: []const u8) !u32 {
+    var valid_passwords: u32 = 0;
+
+    var lines = std.mem.tokenize(input, "\n");
+    while (lines.next()) |line| {
+        var fields = std.mem.tokenize(line, " -:");
+
+        const idx1_field = fields.next() orelse return error.ParseError;
+        const idx2_field = fields.next() orelse return error.ParseError;
+        const char_field = fields.next() orelse return error.ParseError;
+        const password_field = fields.next() orelse return error.ParseError;
+
+        const idx1 = (try std.fmt.parseUnsigned(u32, idx1_field, 10)) - 1;
+        const idx2 = (try std.fmt.parseUnsigned(u32, idx2_field, 10)) - 1;
+        if (char_field.len != 1) return error.ParseError;
+        const char = char_field[0];
+        if (idx1 >= password_field.len or idx2 >= password_field.len) return error.ParseError;
+
+        if (@boolToInt(password_field[idx1] == char) ^
+            @boolToInt(password_field[idx2] == char) == 1)
+            valid_passwords += 1;
+    }
+
+    return valid_passwords;
+}
+
 const test_input =
     \\1-3 a: abcde
     \\1-3 b: cdefg
@@ -72,4 +98,9 @@ const test_input =
 test "findAnswer1" {
     std.testing.expectEqual(@as(u32, 2), try findAnswer1(test_input));
     std.testing.expectEqual(@as(u32, 506), try findAnswer1(@embedFile("input.txt")));
+}
+
+test "findAnswer2" {
+    std.testing.expectEqual(@as(u32, 1), try findAnswer2(test_input));
+    std.testing.expectEqual(@as(u32, 443), try findAnswer2(@embedFile("input.txt")));
 }
