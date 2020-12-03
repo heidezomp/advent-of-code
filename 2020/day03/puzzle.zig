88b172f087b15606194e88221ec66dfe5581538e
heidezomp <heidezomp@protonmail.com>
2020-12-03 13:39:54 +0100

    Day 3, part 2 complete

--- 2020/day03/puzzle.zig
+++ 2020/day03/puzzle.zig
@@ -29,22 +29,40 @@ fn mainArgs(gpa: *Allocator, arena: *Allocator, args: []const []const u8) !void 
     const answer1 = try findAnswer1(input);
     try stdout.print("Answer 1: {}\n", .{answer1});
 
-    //const answer2 = try findAnswer2(input);
-    //try stdout.print("Answer 2: {}\n", .{answer2});
+    const answer2 = try findAnswer2(input);
+    try stdout.print("Answer 2: {}\n", .{answer2});
 }
 
-fn findAnswer1(input: []const u8) !u32 {
-    var trees_encountered: u32 = 0;
+fn traverseMap(input: []const u8, dx: u32, dy: u32) !u32 {
+    std.debug.assert(dy > 0);
 
+    var trees_encountered: u32 = 0;
     var lines = std.mem.tokenize(input, "\n");
     var x: u32 = 0;
     while (lines.next()) |line| {
         if (x >= line.len) return error.ParseError;
         if (line[x] == '#') trees_encountered += 1;
-        x = (x + 3) % @intCast(u32, line.len);
+        x = (x + dx) % @intCast(u32, line.len);
+
+        // Skip line(s) if dy > 1
+        var skipped_y: u32 = 0;
+        while (skipped_y < dy - 1) : (skipped_y += 1)
+            _ = lines.next();
     }
 
     return trees_encountered;
+}
+
+fn findAnswer1(input: []const u8) !u32 {
+    return traverseMap(input, 3, 1);
+}
+
+fn findAnswer2(input: []const u8) !u32 {
+    return (try traverseMap(input, 1, 1)) *
+        (try traverseMap(input, 3, 1)) *
+        (try traverseMap(input, 5, 1)) *
+        (try traverseMap(input, 7, 1)) *
+        (try traverseMap(input, 1, 2));
 }
 
 const test_input =
@@ -64,4 +82,9 @@ const test_input =
 test "findAnswer1" {
     std.testing.expectEqual(@as(u32, 7), try findAnswer1(test_input));
     std.testing.expectEqual(@as(u32, 171), try findAnswer1(@embedFile("input.txt")));
+}
+
+test "findAnswer2" {
+    std.testing.expectEqual(@as(u32, 336), try findAnswer2(test_input));
+    std.testing.expectEqual(@as(u32, 1206576000), try findAnswer2(@embedFile("input.txt")));
 }
