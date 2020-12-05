cbd87ea269ca15ed173f33982ed3fa2aeb5a5f6c
heidezomp <heidezomp@protonmail.com>
2020-12-05 15:07:51 +0100

    Day 5, part 2 complete

--- 2020/day05/puzzle.zig
+++ 2020/day05/puzzle.zig
@@ -13,7 +13,27 @@ pub fn findAnswer1(input: []const u8) !u10 {
 }
 
 pub fn findAnswer2(input: []const u8) !u10 {
-    return error.Unimplemented;
+    // I would like to just use an u1024 and treat it like a big bitset, but
+    // this is unsupported by LLVM. Related bug:
+    // https://github.com/ziglang/zig/issues/1534
+    var occupied_seats: [1024]bool = [1]bool{false} ** 1024;
+
+    var bsps = std.mem.tokenize(input, "\n");
+    while (bsps.next()) |bsp| {
+        const bsp_id = seatBspToId(bsp);
+        occupied_seats[bsp_id] = true;
+    }
+
+    // Find the first occupied seat; the first unoccupied seat after that
+    // should be ours.
+    const first_occupied = for (occupied_seats) |seat, i| {
+        if (seat) break i;
+    } else return error.SeatNotFound;
+    const our_seat = for (occupied_seats[first_occupied..]) |seat, i| {
+        if (!seat) break first_occupied + i;
+    } else return error.SeatNotFound;
+
+    return @intCast(u10, our_seat);
 }
 
 fn seatBspToId(bsp: []const u8) u10 {
@@ -55,4 +75,8 @@ test "seatBspToId" {
 
 test "findAnswer1" {
     std.testing.expectEqual(@as(u10, 832), try findAnswer1(@embedFile("input.txt")));
+}
+
+test "findAnswer2" {
+    std.testing.expectEqual(@as(u10, 517), try findAnswer2(@embedFile("input.txt")));
 }
