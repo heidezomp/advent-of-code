301d841f52c92837539f856f65d16ec880fa2e0e
heidezomp <heidezomp@protonmail.com>
2020-12-06 14:50:57 +0100

    Day 6, part 2 complete

--- 2020/day06/puzzle.zig
+++ 2020/day06/puzzle.zig
@@ -22,7 +22,28 @@ pub fn findAnswer1(input: []const u8) !u32 {
 }
 
 pub fn findAnswer2(input: []const u8) !u32 {
-    return error.Unimplemented;
+    var sum_of_answers: u32 = 0;
+
+    var group_answers: u27 = std.math.maxInt(u27);
+    var lines = std.mem.split(input, "\n");
+    while (lines.next()) |line| {
+        if (line.len == 0) {
+            sum_of_answers += @popCount(u27, group_answers);
+            group_answers = std.math.maxInt(u27);
+            continue;
+        }
+
+        var person_answers: u27 = 0;
+        for (line) |char| {
+            person_answers |= @shlExact(@as(u27, 1), @intCast(u5, char - 'a'));
+        }
+        group_answers &= person_answers;
+    }
+    if (group_answers != std.math.maxInt(u27)) {
+        sum_of_answers += @popCount(u27, group_answers);
+    }
+
+    return sum_of_answers;
 }
 
 const test_input =
@@ -46,4 +67,9 @@ const test_input =
 test "findAnswer1" {
     std.testing.expectEqual(@as(u32, 11), try findAnswer1(test_input));
     std.testing.expectEqual(@as(u32, 6551), try findAnswer1(@embedFile("input.txt")));
+}
+
+test "findAnswer2" {
+    std.testing.expectEqual(@as(u32, 6), try findAnswer2(test_input));
+    std.testing.expectEqual(@as(u32, 3358), try findAnswer2(@embedFile("input.txt")));
 }
