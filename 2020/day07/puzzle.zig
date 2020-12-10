934f7eeb2bf5a8ca10321e712f6e56a04f9c657d
heidezomp <heidezomp@protonmail.com>
2020-12-10 19:51:43 +0100

    Fill is_contained_within hash map

--- 2020/day07/puzzle.zig
+++ 2020/day07/puzzle.zig
@@ -1,37 +1,39 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
 
-const BagName = struct {
-    adjective: []const u8,
-    color: []const u8,
-
-    fn parse(it: *std.mem.TokenIterator) !BagName {
-        const adjective = it.next() orelse return error.ParseError;
-        const color = it.next() orelse return error.ParseError;
-        return BagName{
-            .adjective = adjective,
-            .color = color,
-        };
-    }
-};
+fn parseBagName(it: *std.mem.TokenIterator) ![]const u8 {
+    const adjective = it.next() orelse return error.ParseError;
+    const color = it.next() orelse return error.ParseError;
+    var bag_name = adjective;
+    bag_name.len += 1 + color.len;
+    return bag_name;
+}
 
 pub fn findAnswer1(arena: *Allocator, input: []const u8) !u32 {
-    // TODO This probably won't hash or compare BagNames correctly?
-    const is_contained_within = std.AutoHashMap(BagName, std.AutoHashMap(BagName, void)).init(arena);
+    // Hash map from bag name to set of bag names that directly contain it
+    var is_contained_within = std.StringHashMap(std.StringHashMap(void)).init(arena);
 
+    // Fill the hash map by parsing the input
     var lines = std.mem.tokenize(input, "\n");
     while (lines.next()) |line| {
         std.log.debug("[{}]:", .{line});
 
         var words = std.mem.tokenize(line, " ");
-        const bag_name = BagName.parse(&words);
+        const bag_name = try parseBagName(&words);
         std.log.debug("outer: {}", .{bag_name});
         _ = words.next(); // "bags"
         _ = words.next(); // "contain"
         while (words.next()) |amount_str| {
-            const inner_bag_name = BagName.parse(&words);
+            const inner_bag_name = try parseBagName(&words);
             std.log.debug("inner: {}", .{inner_bag_name});
             _ = words.next(); // "bags"
+
+            // Insert in hash map
+            var result = try is_contained_within.getOrPut(inner_bag_name);
+            if (!result.found_existing) {
+                result.entry.value = std.StringHashMap(void).init(arena);
+            }
+            try result.entry.value.putNoClobber(bag_name, {});
         }
     }
     return error.Unimplemented;
