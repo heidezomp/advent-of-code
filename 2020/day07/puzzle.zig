0443c08380744f3415cb53799ad1c8ca3c001c48
heidezomp <heidezomp@protonmail.com>
2020-12-11 14:43:56 +0100

    Day 7, part 1 (finally!) complete

--- 2020/day07/puzzle.zig
+++ 2020/day07/puzzle.zig
@@ -9,7 +9,7 @@ fn parseBagName(it: *std.mem.TokenIterator) ![]const u8 {
     return bag_name;
 }
 
-pub fn findAnswer1(arena: *Allocator, input: []const u8) !u32 {
+fn constructIsContainedWithin(arena: *Allocator, input: []const u8) !std.StringHashMap(std.StringHashMap(void)) {
     // Hash map from bag name to set of bag names that directly contain it
     var is_contained_within = std.StringHashMap(std.StringHashMap(void)).init(arena);
 
@@ -36,7 +36,35 @@ pub fn findAnswer1(arena: *Allocator, input: []const u8) !u32 {
             try result.entry.value.putNoClobber(bag_name, {});
         }
     }
-    return error.Unimplemented;
+
+    return is_contained_within;
+}
+
+pub fn findAnswer1(arena: *Allocator, input: []const u8) !u32 {
+    const is_contained_within = try constructIsContainedWithin(arena, input);
+
+    // Construct set of all bags that can (in)directly contain a shiny gold bag
+    var query_set = std.StringHashMap(void).init(arena);
+    try query_set.putNoClobber("shiny gold", {});
+    var answer_set = std.StringHashMap(void).init(arena);
+
+    // Restart the iterator each time to prevent iterator invalidation, since
+    // we are going to modify the query set inside the loop
+    while (query_set.iterator().next()) |entry| {
+        const query_bag = entry.key;
+        std.log.debug("processing query bag {}", .{query_bag});
+        query_set.removeAssertDiscard(query_bag);
+        if (is_contained_within.get(query_bag)) |containing_set| {
+            // Add the containing set's members to both the query set and the answer set
+            var containing_iter = containing_set.iterator();
+            while (containing_iter.next()) |containing_entry| {
+                std.log.debug("adding containing bag {}", .{containing_entry.key});
+                try query_set.put(containing_entry.key, {});
+                try answer_set.put(containing_entry.key, {});
+            }
+        }
+    }
+    return answer_set.count();
 }
 
 pub fn findAnswer2(input: []const u8) !u32 {
